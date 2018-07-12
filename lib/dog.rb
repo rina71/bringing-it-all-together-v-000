2db158cbc2deb872d2332180a8cacf940db3abec
Yurina Shin <yurinas@gmail.com>
2018-07-12 01:14:02 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -67,7 +67,7 @@ class Dog
       dog_data = dog[0]
        Dog.new(id: dog_data[0], name:dog_data[1], breed:dog_data[2])
     else
-      self.create(name, breed)
+      self.create(name:name, breed:breed)
     end
   end
 
