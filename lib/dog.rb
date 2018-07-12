b35ebb9a9beafba0164fcce5b5714b7167f1c571
Yurina Shin <yurinas@gmail.com>
2018-07-12 00:28:51 +0000

    automatically backed up by learn

---
+++ lib/dog.rb
@@ -0,0 +1,96 @@
+class Dog
+  attr_accessor :name, :breed, :id
+
+  def initialize(id: nil, name:, breed:)
+    @id = id
+    @name = name
+    @breed = breed
+  end
+
+  def self.create_table
+    sql = <<-SQL
+    CREATE TABLE IF NOT EXISTS dogs (
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      breed TEXT
+    )
+    SQL
+    DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    sql = <<-SQL
+    DROP TABLE dogs
+    SQL
+    DB[:conn].execute(sql)
+  end
+
+  def save
+    sql = <<-SQL
+    INSERT INTO dogs
+    VALUES (?, ?, ?)
+    SQL
+
+    DB[:conn].execute(sql, self.id, self.name, self.breed)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+    self
+
+  end
+
+  def self.create(name:, breed:)
+    dog = Dog.new(name:name, breed:breed)
+    dog.save
+    dog
+
+  end
+
+  def self.find_by_id(id)
+    sql = <<-SQL
+    SELECT *
+    FROM dogs
+    WHERE id = ?
+    SQL
+    DB[:conn].execute(sql,id).map do |row|
+      Dog.new(id:row[0], name:row[1], breed:row[2])
+    end.first
+  end
+
+  def self.find_or_create_by(name:, breed:)
+  
+    if find_by_name(name).name == name
+      find_by_name(name)
+    else
+      self.create(name, breed)
+    end
+  end
+
+  def self.new_from_db
+    sql = <<-SQL
+    SELECT *
+    FROM dogs
+    SQL
+    DB[:conn].execute(sql).map do |row|
+      Dog.new(id:row[0], name:row[1], breed:row[2])
+    end
+    self
+  end
+
+  def self.find_by_name(name)
+    sql = <<-SQL
+    SELECT *
+    FROM dogs
+    WHERE name = ?
+    SQL
+    DB[:conn].execute(sql, name).map do |row|
+      Dog.new(id:row[0], name:row[1], breed:row[2])
+    end.first
+  end
+
+  def update
+    sql = <<-SQL
+    UPDATE dogs
+    SET name = ?, breed = ?
+    SQL
+    DB[:conn].execute(sql, name, breed)
+  end
+end
