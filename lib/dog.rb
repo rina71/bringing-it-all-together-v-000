eef4e06182db93b7a6bdd9c9494097a156ad2395
Yurina Shin <yurinas@gmail.com>
2018-07-12 01:15:59 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -72,14 +72,9 @@ class Dog
   end
 
   def self.new_from_db(row)
-    sql = <<-SQL
-    SELECT *
-    FROM dogs
-    SQL
-    DB[:conn].execute(sql).map do |row|
+  
       Dog.new(id:row[0], name:row[1], breed:row[2])
-    end
-    self
+    
   end
 
   def self.find_by_name(name)
